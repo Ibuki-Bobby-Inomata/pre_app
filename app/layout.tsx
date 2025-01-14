a76856798e5a69c4079a821833bf11da5435d071
猪股一歩希 <ibuki.bobby@gmail.com>
2025-01-14 14:46:13 +0900

    とほほ13まで終了(全部終了)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 
 import './global.css'
+
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'NEXT.JS',
+}
+
 export default function RootLayout({
     children,
 }: {
